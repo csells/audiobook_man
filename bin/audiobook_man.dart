dc58be9b60c15b76fe9b053fa1e3f52615d64852
Chris Sells <csells@sellsbrothers.com>
2024-04-27 13:23:17 -0700

    refactoring

--- bin/audiobook_man.dart
+++ bin/audiobook_man.dart
@@ -2,33 +2,44 @@ import 'dart:io';
 import 'package:dartx/dartx.dart';
 
 void main(List<String> args) async {
-  if (args.length != 1) {
-    print('usage: audiobook_man <mp3 file folder>');
+  if (args.isEmpty || args.length > 2) {
+    print('usage: audiobook_man '
+        '<mp3 file folder> '
+        '[output file name (default: output.mp3)]');
     return;
   }
 
-  final mp3Folder = args[0];
+  final inputFolder = args[0];
+  final outputFile = args.length == 2 ? args[1] : 'output.mp3';
 
   // NOTE: part name wav files from:
   // https://console.cloud.google.com/vertex-ai/generative/speech/text-to-speech
 
-  // ffmpeg -f concat -safe 0 -i mylist.txt -c copy output.mp3
-
-  // create a list of mp3 files from the folder specified by mp3Folder
-  final mp3Files =
-      Directory(mp3Folder).listSync().where((f) => f.path.endsWith('.mp3'));
-
-  // create a temp file to hold the file list using' the OS temp folder
-  final tempDir = await Directory.systemTemp.createTemp('audiobook_man');
-  final fileList = File('${tempDir.path}/filelist.txt');
-  final lines = mp3Files.map((n) =>
-      "file ${n.path.replaceAll("'", r"\'").replaceAll(" ", r"\ ")}\n");
-  await fileList.writeAsString(lines.sorted().join(''));
-
   // execute ffmpeg to concatenate the files in the list into a single mp3
-  final results = await Process.run(
-    'ffmpeg',
-    [
+  final ffmpeg = Ffmpeg();
+  ffmpeg.concatFolder(inputFolder: inputFolder, outputFile: outputFile);
+}
+
+class Ffmpeg {
+  final _exe = 'ffmpeg';
+
+  void concatFolder({
+    required String inputFolder,
+    required String outputFile,
+  }) async {
+    // create a list of mp3 files from the folder specified by mp3Folder
+    final mp3Files =
+        Directory(inputFolder).listSync().where((f) => f.path.endsWith('.mp3'));
+
+    // create a temp file to hold the file list using' the OS temp folder
+    final tempDir = await Directory.systemTemp.createTemp('audiobook_man');
+    final fileList = File('${tempDir.path}/filelist.txt');
+    final lines = mp3Files.map((n) =>
+        "file ${n.path.replaceAll("'", r"\'").replaceAll(" ", r"\ ")}\n");
+    await fileList.writeAsString(lines.sorted().join(''));
+
+    // ffmpeg -f concat -safe 0 -i mylist.txt -c copy output.mp3
+    final args = [
       '-f',
       'concat',
       '-safe',
@@ -37,13 +48,23 @@ void main(List<String> args) async {
       fileList.path,
       '-c',
       'copy',
-      'output.mp3',
-    ],
-    runInShell: true,
-  );
+      outputFile,
+    ];
 
-  print(results.stderr);
+    try {
+      final results = await Process.run(_exe, args);
 
-  // delete the temp folder
-  await tempDir.delete(recursive: true);
+      if (results.exitCode != 0) {
+        throw ProcessException(
+          _exe,
+          args,
+          results.stderr.toString(),
+          results.exitCode,
+        );
+      }
+    } finally {
+      // delete the temp folder
+      await tempDir.delete(recursive: true);
+    }
+  }
 }
